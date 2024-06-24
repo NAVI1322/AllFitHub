61709e0a11da95ed13f16a9a5c043a869a2e016c
NAVI1322 <rksharma041976@gmail.com>
2024-06-24 18:17:50 -0400

    dish modal added

---
+++ frontend/src/pages/Diet/diet.tsx
@@ -0,0 +1,13 @@
+
+import DietCard from "@/components/layout/Diet_card";
+
+
+const DietPage= () => {
+    return ( 
+        <div>
+            <DietCard/>
+        </div>
+    );
+}
+ 
+export default DietPage;
